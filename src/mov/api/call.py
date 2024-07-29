90ca96a88a80ba26c43d3612ae81b890058e26aa
sooj1n <sujin19371@gmail.com>
2024-07-29 15:38:57 +0900

    aa

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -2,8 +2,9 @@ import requests
 import os
 import pandas as pd
 
-def save2df():
-    df = list2df()
+def save2df(load_dt='20120101'):
+    """airflow 호출 지점"""
+    df = list2df(load_dt)
     # df에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식으로)
     # 아래 파일 저장 시 load_dt 기분으로 파티셔닝
     df['load_dt'] = '20120101'
@@ -11,13 +12,13 @@ def save2df():
     df.to_parquet('~/tmp/test_parquet',partition_cols=['load_dt'])
     return df
 
-def list2df():
-    l = req2list()
+def list2df(load_dt='20120101'):
+    l = req2list(load_dt)
     df = pd.DataFrame(l)
     return df
 
-def req2list() -> list:
-    _,data = req()
+def req2list(load_dt='20120101') -> list:
+    _,data = req(load_dt)
     l=data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
@@ -26,19 +27,19 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req(dt="20120101"):
+def req(load_dt="20120101"):
    # url = gen_url('20240720')
-    url = gen_url(dt)
+    url = gen_url(load_dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
     print(data)
     return code, data
 
-def gen_url(dt="20120101"):
+def gen_url(load_dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
-    url= f"{base_url}?key={key}&targetDt={dt}"
+    url= f"{base_url}?key={key}&targetDt={load_dt}"
     
     return url
 
--- tests/test_call.py
+++ tests/test_call.py
@@ -3,10 +3,11 @@ import pandas as pd
 
 
 def test_save2df():
-    df = save2df()
+    df = save2df(load_dt='20241231')
     assert isinstance(df, pd.DataFrame)
     assert 'load_dt' in df.columns
 
+    
 def test_list2df():
     df = list2df()
     print(df)
@@ -30,6 +31,9 @@ def test_gnt_url():
     #assert url == "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=8ece39e938f850f4099b0d6f34095640&targetDt=20120101"
     assert "http" in url
     assert "kobis" in url
+    
+    url = gen_url('20120101')
+    
 
 def test_req():
     code, data = req()
